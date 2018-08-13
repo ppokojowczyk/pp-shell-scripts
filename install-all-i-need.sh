ec3ce9e9ee8cb444a389fe4987996b107059debd
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2018-08-13 18:53:57 +0200

    added glances to install-all-i-need

--- install-all-i-need.sh
+++ install-all-i-need.sh
@@ -6,7 +6,7 @@
 
 # Packages to be installed
 PACKAGES='vsftpd git unrar unace p7zip nfs-common nfs-kernel-server atop redshift whois dnsmasq shutter geeqie python python-pip bluefish gtypist klavaro skypeforlinux bluefish gimp gnome-commander redshift-gtk emma tora parcellite adminer phpmyadmin krusader filezilla codeblocks codelite kdevelop x11vnc rofi vim vim-gtk numix-gtk-theme build-essential autoconf automake pkg-config libgtk-3.0 libgtk-3-dev shimmer-themes ppa-purge composer nodejs npm dia ntp
-wireshark compton xbindkeys meld ssmtp gphoto2 ncftp pastebinit baobab fonts-inconsolata fonts-hack-ttf smartmontools stress glmark2 testdisk audacious audacity krita mypaint inkscape libreoffice mono-complete keepassx qnapi subdownloader subliminal gcolor2 mc xpad terminator xfburn geany simple-scan chromium-browser nautilus-dropbox firefox hexchat pidgin remmina thunderbird transmission smplayer vlc evince conky vagrant virtualbox aptitude xfonts-terminus i3 j4-dmenu-desktop tmux giggle feh sublime-text arandr'  
+wireshark compton xbindkeys meld ssmtp gphoto2 ncftp pastebinit baobab fonts-inconsolata fonts-hack-ttf smartmontools stress glmark2 testdisk audacious audacity krita mypaint inkscape libreoffice mono-complete keepassx qnapi subdownloader subliminal gcolor2 mc xpad terminator xfburn geany simple-scan chromium-browser nautilus-dropbox firefox hexchat pidgin remmina thunderbird transmission smplayer vlc evince conky vagrant virtualbox aptitude xfonts-terminus i3 j4-dmenu-desktop tmux giggle feh sublime-text arandr unclutter glances'
 
 # Use this package manager command
 PACKAGE_MANAGER_COMMAND='apt-get install'

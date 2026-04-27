6fbe93cbbe4a839ae819eff770132352ecaf1d1b
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2026-04-27 18:55:14 +0200

    update toggle-display script

--- toggle-display.sh
+++ toggle-display.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
 
-MSG=""
+DEFAULT_DPI=96
+NEW_DISPLAY=""
+NEW_DPI=$DEFAULT_DPI
+#LARGER_DPI=144
+LARGER_DPI=192
+ALL_DISPLAYS=($(xrandr | awk '/ connected/{print $1}'))
 CURRENT_DISPLAY=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}')
-NEW_DISPLAY="DP-1"
-NEW_DPI=144
+DEFAULT_DISPLAY=""
 
 notify () {
     notify-send -t 10000 "$1"
 }
 
+#CURRENT_DISPLAY="HDMI7"
+#ALL_DISPLAYS=(
+#    DP1
+#    CRT2
+#    HDMI7
+#    DP34
+#    CRT-01
+#);
+CT=0
+
+# Find next display.
+for i in ${ALL_DISPLAYS[@]}; do
+    let CT=$CT+1
+    if [[ $i == $CURRENT_DISPLAY ]]; then
+        if [[ -n ${ALL_DISPLAYS[$CT]} ]]; then
+            NEW_DISPLAY=${ALL_DISPLAYS[$CT]}
+        else
+            NEW_DISPLAY=${ALL_DISPLAYS[0]}
+        fi
+        break
+    fi
+done
+
+if [[ -z $NEW_DISPLAY ]]; then
+    NEW_DISPLAY=${ALL_DISPLAYS[0]}
+fi
+
 # Display.
 
-if [[ $CURRENT_DISPLAY == 'DP-1' ]];then
-    NEW_DISPLAY="eDP-1"
-fi
-
-if [[ -z $NEW_DPI || -z $NEW_DISPLAY ]]; then
-    notify "Can't determine new display or DPI."
+if [[ -z $NEW_DISPLAY ]]; then
+    notify "Can't determine new display."
     exit 1
-fi
-
-if [[ $NEW_DISPLAY == $CURRENT_DISPLAY ]]; then
-    return 0
 fi
 
 notify "Enabling $NEW_DISPLAY"
@@ -30,17 +53,20 @@ xrandr --output $NEW_DISPLAY --auto
 
 # DPI.
 
-X_RES=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $4}' | sed 's/\([[:digit:]]*\).*/\1/')
+if [ $XDG_SESSION_DESKTOP == 'i3' ]; then
+    X_RES=$(xrandr | awk '/ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $4}' | sed 's/\([[:digit:]]*\).*/\1/')
 
-if [[ -z $X_RES ]]; then
-    notify "Can't determine new X resolution."
+    if [[ -z $X_RES ]]; then
+        notify "Can't determine new X resolution."
+    fi
+
+    if [[ -n $X_RES && $X_RES -gt 1920 ]]; then
+        NEW_DPI=$LARGER_DPI
+    fi
+
+    echo "Xft.dpi: $NEW_DPI" | xrdb -override -
+
+    notify "New DPI: $NEW_DPI"
+    nitrogen --restore # Restore the desktop wallpaper as it can be rendered incorrectly.
 fi
-
-if [[ -n $X_RES && $X_RES -gt 1920 ]]; then
-    NEW_DPI=192
-fi
-
-echo "Xft.dpi: $NEW_DPI" | xrdb -override -
-
-notify "New DPI: $NEW_DPI"
 
